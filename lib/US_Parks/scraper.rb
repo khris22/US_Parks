51c5d2fb6b533afd90923269a7e36d385e4be8cc
Khris Punzalan <khrispunzalan@gmail.com>
2019-08-02 05:52:12 -0700

    scrape state name & url

--- lib/US_Parks/scraper.rb
+++ lib/US_Parks/scraper.rb
@@ -1,3 +1,20 @@
 class USParks::Scraper
 
+    def self.scrape_state
+        doc = Nokogiri::HTML(open("https://www.nps.gov/index.htm"))
+        # array_states_list = doc.css("li").children.map{ |name| name.text }
+        #     parse_state_name = array_states_list.values_at(*5..58)
+
+        array_states_list = doc.css("a").children.map{ |name| name.text.strip}
+            parse_state_name = array_states_list.values_at(*15..70)
+
+        array_state_url = doc.css("a").map{|link| link['href']}
+            parse_state_url = array_state_url.values_at(*7..60)
+
+        attributes = {name: parse_state_name, state_url: parse_state_url}
+        binding.pry
+
+    end
+
+
 end
